1b8eca27c881d865f808713e3df8bee2e50776ea
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-02-21 13:42:57 +0530

    Add files via upload

---
+++ exercise12.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>Student Details</title>
+	<style>
+		td{ text-align: center; }
+	</style>
+</head>
+<body>
+	<h1 align="center">STUDENT DETAILS</h1>
+	<?php
+		$host = "localhost";
+		$user = "root";
+		$pwd = "";
+		$db = "webdb";
+		$conn = mysqli_connect($host, $user, $pwd, $db);
+		if(!$conn)
+			echo "Database connection failed!!!";
+		else
+		{
+			$result = mysqli_query($conn,"select * from student order by rollno");
+			echo "<table border='1' align='center' width='50%' cellpadding='10' cellspacing='5'><tr><th>Roll no</th><th>Name</th><th>Gender</th><th>Mark</th></tr>";
+			while($row = mysqli_fetch_assoc($result))
+			{
+				echo "<tr><td>".$row["rollno"]."</td><td>".$row["name"]."</td><td>";
+				if($row["gender"]=='M')
+					echo "MALE</td><td>";
+				else if($row["gender"]=='F')
+					echo "FEMALE</td><td>";
+				echo $row["mark"]."</td></tr>";
+			}
+			mysqli_close($conn);
+			echo "</table>";
+		}
+	?>
+</body>
+</html>
