b1fc28bef8543cb044a38088fd15de376b60bc39
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-02-21 16:42:58 +0530

    Update exercise12.php

--- exercise12.php
+++ exercise12.php
@@ -18,19 +18,25 @@
 		else
 		{
 			$result = mysqli_query($conn,"select * from student order by rollno");
-			echo "<table border='1' align='center' width='50%' cellpadding='10' cellspacing='5'><tr><th>Roll no</th><th>Name</th><th>Gender</th><th>Mark</th></tr>";
-			while($row = mysqli_fetch_assoc($result))
+			if(mysqli_num_rows($result))
 			{
-				echo "<tr><td>".$row["rollno"]."</td><td>".$row["name"]."</td><td>";
-				if($row["gender"]=='M')
-					echo "MALE</td><td>";
-				else if($row["gender"]=='F')
-					echo "FEMALE</td><td>";
-				echo $row["mark"]."</td></tr>";
+				echo "<table border='1' align='center' width='50%' cellpadding='10' cellspacing='5'><tr><th>Roll no</th><th>Name</th><th>Gender</th><th>Mark</th></tr>";
+				while($row = mysqli_fetch_assoc($result))
+				{
+					echo "<tr><td>".$row["rollno"]."</td><td>".$row["name"]."</td><td>";
+					if($row["gender"]=='M')
+						echo "MALE</td><td>";
+					else if($row["gender"]=='F')
+						echo "FEMALE</td><td>";
+					echo $row["mark"]."</td></tr>";
+				}
+				echo "</table>";
 			}
+			else
+				echo "<center>No student in database</center>";
 			mysqli_close($conn);
-			echo "</table>";
 		}
 	?>
 </body>
+</html>
 </html>
