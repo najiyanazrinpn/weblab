8f3bab65785cf5dfbb9b9f3ac5a72f2432549ff6
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-03-24 07:06:37 +0530

    Delete LIBRARY directory

--- LIBRARY/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root";
-	$pwd = "";
-	$db = "library";
-	$conn = mysqli_connect($host, $user, $pwd, $db);
-	if(!$conn)
-		echo "Database connection failed!!!";
-?>
--- LIBRARY/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	session_unset();
-	session_destroy();
-	echo "<script>alert(`You\'ve been logged out!!!`); window.location.href='login.php';</script>";
-?>
